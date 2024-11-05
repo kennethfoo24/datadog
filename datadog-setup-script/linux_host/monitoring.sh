f48f70de458a83d1bbc438fd5838237eeab15cb2
Kenneth Foo <101826160+kennethfoo24@users.noreply.github.com>
2024-11-05 19:19:32 +0800

    Update monitoring.sh

--- datadog-setup-script/linux_host/monitoring.sh
+++ datadog-setup-script/linux_host/monitoring.sh
@@ -168,15 +168,15 @@ logs:
     service: $SERVICE_NAME
     source: $SOURCE_NAME
   - type: file
-    path: /*/**/*.log
+    path: /**/**/*.log
     service: $SERVICE_NAME
     source: $SOURCE_NAME
   - type: file
-    path: /*/**/**/*.log
+    path: /**/**/**/*.log
     service: $SERVICE_NAME
     source: $SOURCE_NAME
   - type: file
-    path: /*/**/**/**/*.log
+    path: /**/**/**/**/*.log
     service: $SERVICE_NAME
     source: $SOURCE_NAME
 EOF
